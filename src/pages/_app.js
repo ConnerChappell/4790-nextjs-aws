4e1a5becaf6d3682a8c1d29884dfb219e882b937
Conner Chappell <conner.chappell123@gmail.com>
2022-04-03 23:20:33 -0600

    Added auth with aws and cognito

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { Amplify, Auth } from 'aws-amplify'
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+import config from '../aws-exports'
+
+Amplify.configure(config)
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default withAuthenticator(MyApp)
