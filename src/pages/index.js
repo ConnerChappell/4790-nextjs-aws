bbe3ecf993e37728bad0e805ad11305f46aa27a4
Conner Chappell <conner.chappell123@gmail.com>
2022-04-04 18:22:08 -0600

    Added user attribute to home page when signed in

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import Head from 'next/head'
+import { useAuthenticator } from '@aws-amplify/ui-react'
 import { Typography } from '@mui/material'
 
 export default function Home() {
+    const { user } = useAuthenticator((context) => [context.user])
+    console.log(user)
+
     return (
         <div>
             <Head>
@@ -10,7 +14,7 @@ export default function Home() {
 
             <main>
             <Typography variant='h3' component='h1' sx={{textAlign: 'center',}}>
-                    Welcome to my Next.js Sports App
+                    Welcome to my Next.js Sports App {user.attributes.preferred_username}
             </Typography>
             </main>
         </div>
